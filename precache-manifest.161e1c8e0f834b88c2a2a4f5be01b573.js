69b5154aba149c53a70914f55b5716b162384bb1
Axel Máni <selii93@gmail.com>
2020-08-02 23:54:02 +0000

    Updates

---
+++ precache-manifest.161e1c8e0f834b88c2a2a4f5be01b573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae93fc1af7eefe0d4716f46f7189cf2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae2a86f8aa28f251cb5",
+    "url": "/static/css/main.4d29e8e4.chunk.css"
+  },
+  {
+    "revision": "31f3e3ccd561a73ff77b",
+    "url": "/static/js/2.b1897d45.chunk.js"
+  },
+  {
+    "revision": "f22db956f020603d453b0221094e42c2",
+    "url": "/static/js/2.b1897d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae2a86f8aa28f251cb5",
+    "url": "/static/js/main.b7e3c667.chunk.js"
+  },
+  {
+    "revision": "cde03898896ae393bca7",
+    "url": "/static/js/runtime-main.b0faf926.js"
+  },
+  {
+    "revision": "4dd664c66391b25185c7030875b5d709",
+    "url": "/static/media/logo.4dd664c6.svg"
+  }
+]);
