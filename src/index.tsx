00c8956609b5b9f069b20f3c87eb192aa6ddc8ff
Axel Máni <selii93@gmail.com>
2020-08-04 13:33:50 +0000

    changing primary and secondary colors

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import ReactGA from 'react-ga';
 import './i18n';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#005a9c',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#9a9a9a',
+    },
+  },
+});
 
 export const initGA = () => {
   ReactGA.initialize('UA-173566706-1');
@@ -16,7 +31,9 @@ export const GApageView = (page = '/') => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
